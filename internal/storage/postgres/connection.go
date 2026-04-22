fb17beec02f8dbd3233dfe731485d556ef8e3efb
Aaron Gibran <aarongibran13@gmail.com>
2026-04-22 12:15:58 -0300

    feat(storage): implement postgres connection with GORM

--- internal/storage/postgres/connection.go
+++ internal/storage/postgres/connection.go
@@ -1,1 +1,26 @@
-package postegres
+package postgres
+
+import (
+	"github.com/Aaron-GMM/DockOps/internal/config/logger"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var log = logger.NewLogger("DataBase")
+
+func Connect(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.ErrorF("Erro ao conectar no banco de dados: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&EventModel{})
+	if err != nil {
+		log.ErrorF("Erro ao executar AutoMigrate: %v", err)
+		return nil, err
+	}
+
+	log.ErrorF("Conexão com PostgreSQL estabelecida com sucesso!")
+	return db, nil
+}
