ae25f962b7da9fb1aaf2a2580b8ed9ff9490a1ac
Aaron Gibran <aarongibran13@gmail.com>
2026-04-23 17:47:26 -0300

    refactor(Logger): add '...' in value the logs formatted

--- internal/config/logger/logger.go
+++ internal/config/logger/logger.go
@@ -40,14 +40,14 @@ func (L *Logger) Error(v ...interface{}) {
 }
 
 func (L *Logger) Debugf(format string, v ...interface{}) {
-	L.debug.Printf(format, v)
+	L.debug.Printf(format, v...)
 }
 func (L *Logger) InforF(format string, v ...interface{}) {
-	L.info.Printf(format, v)
+	L.info.Printf(format, v...)
 }
 func (L *Logger) WarningF(format string, v ...interface{}) {
-	L.warning.Printf(format, v)
+	L.warning.Printf(format, v...)
 }
 func (L *Logger) ErrorF(format string, v ...interface{}) {
-	L.error.Printf(format, v)
+	L.error.Printf(format, v...)
 }
