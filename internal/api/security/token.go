0cf34cdd6a42b63efcb34c78cc2d51047b827d3b
Aaron Gibran <aarongibran13@gmail.com>
2026-05-02 14:34:59 -0300

    feat(security): implementa parserToken and test AAA.

--- internal/api/security/token.go
+++ internal/api/security/token.go
@@ -1,6 +1,9 @@
 package security
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/Aaron-GMM/DockOps/internal/config/logger"
@@ -28,4 +31,44 @@ func GenerateToken(userID string, role string, secret string) (string, error) {
 
 	log.InforF("Token gerado com sucesso para o ID: %s", userID)
 	return tokenString, nil
+}
+
+func ParseToken(authHeader http.Header, secret string) (jwt.MapClaims, error) {
+	authStr := authHeader.Get("Authorization")
+	if authStr == "" {
+		log.WarningF("Authorization header is missing")
+		return nil, fmt.Errorf("authorization header is missing")
+	}
+
+	parts := strings.Split(authStr, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		log.WarningF("Invalid authorization header format")
+		return nil, fmt.Errorf("invalid authorization header format")
+	}
+	tokenString := parts[1]
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			log.ErrorF("Signing method is not valid: %v", token.Header["alg"])
+			return nil, fmt.Errorf("invalid signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		log.WarningF("Error Parsing token: %v", err)
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if sub, ok := claims["sub"].(string); ok {
+			log.Debugf("Token to ID: %s Token is Valid and Success", sub)
+		} else {
+			log.Debugf("Token is Valid and Success, but missing 'sub'")
+		}
+		return claims, nil
+	}
+
+	log.WarningF("Invalid token claims format")
+	return nil, fmt.Errorf("invalid token claims format")
 }
